b27480189dc9415de5bb8252d703ab7add257542
johannborges <johannborges@hotmail.com>
2017-06-25 16:15:18 -0300

    novo site

---
+++ talentos/Lista de Talentos - Tormenta RPG_arquivos/main.js
@@ -0,0 +1,61 @@
+Main = {
+	init: function(){
+        $("#search").on("keyup", function(){
+            var _searchText = $(this).val().trim().toLowerCase();
+            
+            if(_searchText.length <= 0)
+                $(".talento").show(0);
+            
+            else{
+                $(".talento").each(function(){
+                    var _this = $(this);
+                    var _text = _this.text().trim().toLowerCase();
+
+                    if(_text.indexOf(_searchText) !== -1)
+                        _this.show(0);
+                    else
+                        _this.hide(0);
+                });
+            }
+        });
+        
+        $("#top_bar .clear").on("click", function(){
+            $("#search").val("");
+            $(".talento").show(0);
+        });
+        
+		$(".talento .close").on("click", function(){
+			$(this).closest(".talento").toggleClass("closed");
+		});
+
+		$("#top_bar a:not(.logo)").on("click", function(e){
+            e.preventDefault();
+
+			var _id = $(this).attr("href");
+            
+			$("html, body").animate({
+				scrollTop: $(_id).offset().top - 100
+			}, 300);
+        });
+
+		$("#middle a").on("click", function(e){
+            e.preventDefault();
+
+			var _id = $(this).attr("href");
+            
+			$("html, body").animate({
+				scrollTop: $(_id).offset().top - 100
+			}, 300);
+            
+            setTimeout(function(){
+                $(_id).addClass("blink");
+            }, 450);
+
+            setTimeout(function(){
+                $(_id).removeClass("blink");
+            }, 1500);
+		});
+	}
+}
+
+$(Main.init);
